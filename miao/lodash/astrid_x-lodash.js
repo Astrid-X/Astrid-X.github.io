2818ecf6c5f58188672a5aef5260f057385c2c33
Astrid-X <38812370+Astrid-X@users.noreply.github.com>
2018-07-12 21:16:42 +0800

    Create astrid_x-lodash.js

---
+++ miao/lodash/astrid_x-lodash.js
@@ -0,0 +1,34 @@
+var astrid_x = {
+  chunk: function(array, size) {
+    var result = []
+    for (var i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, size + i))
+    }
+    return result
+  }
+
+  compact: function(array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  }
+
+  difference: function (array,...value) {
+    let res = []
+    let val = value[0]
+    for (let i = 1; i < value.length; i++) {
+      val = val.concat(value[i])
+    }
+    for (let k = 0;k < array.length; k++) {
+      if (!val.includes(array[k])) {
+        res.push(array[k])
+      }
+    }
+    return res
+  }
+}
+
