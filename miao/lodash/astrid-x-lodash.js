ed7c605eeea692eb4a8dfd2445cf91f474013bc8
Astrid-X <38812370+Astrid-X@users.noreply.github.com>
2018-07-12 14:55:18 +0800

    Update astrid-x-lodash.js

--- miao/lodash/astrid-x-lodash.js
+++ miao/lodash/astrid-x-lodash.js
@@ -1,11 +1,21 @@
 var astrid_x = {
   chunk: function(array, size) {
-
+    var result = []
+    for (var i = 0; i < array.length; i += size) {
+      result.push(array.slice(i, size + i))
+    }
+    return result
   }
 }
 
 var astrid_x = {
   compact: function(array) {
-
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
   }
 }
