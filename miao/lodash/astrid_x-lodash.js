c0f223cb5c02294911a1363b419fb74074f67c68
Astrid-X <38812370+Astrid-X@users.noreply.github.com>
2018-07-18 09:44:27 +0800

    Update astrid_x-lodash.js

--- miao/lodash/astrid_x-lodash.js
+++ miao/lodash/astrid_x-lodash.js
@@ -135,17 +135,18 @@ var astrid_x = {
   },
 
   pull:
-  function(array, ...value) {
-    for (var i = array.length; i >= 0; i--) {
-      for (var j = value.length; j >= 0; j--) {
-        if (array[i] === value[j]) {
-          array.slice(i, 1)
+  function(array, ...arr) {
+    var arrrayLen = array.length
+    var arrLen = arr.length
+    for (var i = arrrayLen; i >= 0; i--) {
+        for (var j = arr.length; j >= 0; j--) {
+            if (array[i] === arr[j]) {
+                array.splice(i, 1)
+            }
         }
-      }
     }
     return array
-  },
+  },  
 
-  
 }
 
